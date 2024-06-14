05294d3039dde0eb161fcc28e3192ab6dec11320
YASHAS R <YASHAS R@LAPTOP-L00T6CHA>
2024-06-14 10:09:50 +0530

    first commit

---
+++ B94java/src/edu/atria/b94java/secondpackage/ExecutorTwo.java
@@ -0,0 +1,24 @@
+package edu.atria.b94java.secondpackage;
+
+import edu.atria.b94java.firstpackage.Base;
+
+
+public class ExecutorTwo extends Base {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Base bTwo = new Base();
+		//private members are not accessible outside the first package
+		//System.out.println(bTwo.variablePriavte);
+		
+		//default members are not accessible outside the first package
+		//System.out.println(bTwo.variableDeafult);
+		
+		ExecutorTwo ex = new ExecutorTwo();
+		System.out.println(ex.variableProtected);
+		
+		System.out.println(bTwo.variablePublic);
+
+	}
+
+}
